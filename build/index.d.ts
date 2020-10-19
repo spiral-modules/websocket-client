e9f7d0ca526f1859e827ef3366ef5f78e35e9f35
Andrew Kirkovski <andrew@spiralscout.com>
2020-10-19 10:05:41 +0300

    update to 0.0.4

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,10 +1,11 @@
 import { SFSocket } from './SFSocket';
 import { NamesDict as eventNames } from './eventdispatcher/events';
+import Channel, { ChannelStatus } from './Channel';
+import { ICallback, ICallbackTable, SFEventMap, UEventCallback, UndescribedCallbackFunction } from './types';
 declare const makeSocketOptions: (wsUrl: string) => {
     host: string;
     port: string;
     path: string;
 } | null;
-export { SFSocket, makeSocketOptions, eventNames };
-export default SFSocket;
+export { SFSocket, makeSocketOptions, eventNames, Channel, ChannelStatus, ICallback, ICallbackTable, SFEventMap, UEventCallback, UndescribedCallbackFunction, };
 //# sourceMappingURL=index.d.ts.map
