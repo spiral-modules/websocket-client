20f8cf009aeb029c8ae5ba83862ebedb6272a262
Andrei Kirkouski <andrew@spiralscout.com>
2022-10-20 13:23:08 +0400

    retry strategy

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,10 +1,12 @@
 import { ISFSocketConfig } from './SFSocket';
+import { retryStrategy } from './retrystrategy';
 
 export const defaultConfig: ISFSocketConfig = {
   host: '',
   port: 80,
   path: '',
   unavailableTimeout: 10000,
-  retryTimeout: 1000,
+  retryTimeout: 10,
   useTLS: false,
+  retryStrategy: retryStrategy({ retries: 3, delay: 1000, multiplier: 5 }),
 };
